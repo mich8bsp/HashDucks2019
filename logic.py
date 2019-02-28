c7b09ef72cda9a61e526d73f915dff5a71d0b7cb
nircoh <99nirc99@gmail.com>
2019-02-28 20:24:34 +0200

    score logic done

--- logic.py
+++ logic.py
@@ -3,6 +3,28 @@ from input_state import InputState
 from output_state import OutputState
 import knapsack
 
+
+def calc_pair_score(slide1, slide2):
+    intersections = len(slide1.tags.intersection(slide2.tags))
+
+    if intersections > slide1.len():
+        if len(slide1.tags) > len(slide2.tags):
+            return len(slide2.tags)
+        return len(slide1.tags)
+    if intersections > len(slide2):
+        return len(slide2.tags)
+    return intersections
+
+
+def calc_score(slides):
+    score = 0
+    for index, slide in enumerate(slides):
+        if slides[index + 1] is not None:
+            score += calc_pair_score(slide, slides[index + 1])
+
+    return score
+
+
 def run_logic(input_state):
     images = input_state.images
     if len(images == 1):
