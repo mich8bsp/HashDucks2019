63afd6be4f37c655ce6d9f23162231f9f2333b79
Michael Bespalov <michael.bespalov@genoox.com>
2019-02-28 18:15:35 +0200

    env init

--- io_parser.py
+++ io_parser.py
@@ -2,9 +2,11 @@ import os
 
 from output_state import OutputState
 from input_state import InputState
+import time
 
 
 def parse_input_file(file_path):
+    # type: (str) -> InputState
     with open(os.path.join("input_files", file_path)) as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines]
@@ -17,11 +19,12 @@ def build_input_state(input_lines):
 
 
 def write_output_to_file(output_state):
-    import time
+    # type: (OutputState) -> None
     millis = int(round(time.time() * 1000))
     with open(os.path.join("output_files", "output." + str(millis) + ".txt"), "w+") as out_file:
         out_file.writelines("\n".join(get_out_lines(output_state)))
 
 
 def get_out_lines(output_state):
+    # type: (OutputState) -> [str]
     return ["abc"]
--- logic.py
+++ logic.py
@@ -1,5 +1,8 @@
+from input_state import InputState
 from output_state import OutputState
 
 
 def run_logic(input_state):
+    # type: (InputState) -> OutputState
+
     return OutputState()
