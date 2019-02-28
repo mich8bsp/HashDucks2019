b078ba1d8f4e86085e1c93915af17045c7591457
Jonathan <jonathan.shifman@gmail.com>
2019-02-28 20:05:59 +0200

    Jonathan first commit

--- input_state.py
+++ input_state.py
@@ -1,2 +1,7 @@
 class InputState(object):
-    pass
+
+    def __init__(self):
+        self.images = []
+
+    def add_image(self, image):
+        self.images.append(image)
--- logic.py
+++ logic.py
@@ -3,6 +3,17 @@ from output_state import OutputState
 
 
 def run_logic(input_state):
-    # type: (InputState) -> OutputState
-
-    return OutputState()
+    images = input_state.images
+    if len(images == 1):
+        image = images[0]
+        if image.orientation == Orientation.VERTICAL:
+            raise Exception("WTF")
+        output_state = OutputState([image.id])
+    else:
+        if images[0].orientation == Orientation.HORIZONTAL:
+            output_state = OutputState([images[0].id])
+        elif images[1].orientation == Orientation.HORIZONTAL:
+            output_state = OutputState([images[1].id])
+        else:
+            output_state = OutputState([images[0].id], images[1].id)
+    return output_state
